0b0c44a324dc95be3fe8e704cf5985374d4aa166
chihunmanse <chihunmanse@gmail.com>
2021-10-13 11:13:04 +0900

    [ADD]HHYYY carts-views

--- carts/models.py
+++ carts/models.py
@@ -1,11 +1,10 @@
 from django.db                      import models
 
-from products.models                import Product, Option
+from products.models                import Option
 from users.models                   import User
 
 class Cart(models.Model) :
     user     = models.ForeignKey('users.User', on_delete = models.CASCADE)
-    product  = models.ForeignKey('products.Product', on_delete = models.CASCADE)
     option   = models.ForeignKey('products.Option', on_delete = models.CASCADE)
     quantity = models.PositiveIntegerField(default = 0)
 
--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,8 @@
 from django.urls            import path
 
-from products.views         import ProductView, ProductDetailView, NavigatorView
+from products.views         import ProductView, ProductDetailView
 
 urlpatterns = [
     path('', ProductView.as_view()),
     path('/<int:product_id>', ProductDetailView.as_view()),
-    path('/navigator/<int:category_id>', NavigatorView.as_view())
 ]
