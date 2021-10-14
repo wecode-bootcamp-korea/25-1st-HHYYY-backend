507c372b55f7d54bfd0c830702486e5aaa824929
JiHoon KIM <jihoonkim@JiHoonui-MacBookAir.local>
2021-10-14 10:46:32 +0900

    [MODIFY] reviews-urls

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,10 @@
 from django.urls            import path
 
-from products.views         import ProductView, ProductDetailView, NavigatorView
+from products.views         import ProductView, ProductDetailView
 from reviews.views          import ReviewView
 
 urlpatterns = [
     path('', ProductView.as_view()),
     path('/<int:product_id>', ProductDetailView.as_view()),
-    path('/reviews/<int:category_id>', ReviewView.as_view())
+    path('/reviews/<int:product_id>', ReviewView.as_view()),
 ]
--- reviews/urls.py
+++ reviews/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from reviews.views import ReviewDetailView, ReviewLikeView, ReviewCommentView, ReivewCommentDetailView  
+from reviews.views import ReviewDetailView, ReviewLikeView, ReviewCommentView, ReviewCommentDetailView
 
 urlpatterns = [
     path('/<int:review_id>', ReviewDetailView.as_view()),
     path('/comments/<int:review_id>', ReviewCommentView.as_view()),
-    path('/comment/<int:review_id>', ReivewCommentDetailView.as_view()),
-    path('/like/<int:review_id>', ReviewLikeView.as_view())
+    path('/comment/<int:comment_id>', ReviewCommentDetailView.as_view()),
+    path('/like/<int:review_id>', ReviewLikeView.as_view()),
 ]
